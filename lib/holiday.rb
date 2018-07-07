516593b5782777b6a20a0821ebd1d541bb2f2523
Sarah Jacob <smjacob91@gmail.com>
2018-07-07 13:51:36 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -42,11 +42,34 @@ def add_supply_to_memorial_day(holiday_hash, supply)
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
-  # code here
-  # remember to return the updated hash
+  new_holiday = holiday_hash[:fall].push(:columbus_day)
+
 
 end
 
+
+# Question 4
+  # Write a method that adds a new holiday and its associated supplies to any season
+  describe "#add_new_holiday_with_supplies" do
+    it "modifies the original hash by adding supplies of a new holiday to a season" do
+      expect(holiday_supplies[:fall].keys).not_to include(:columbus_day)
+
+      columbus_day_supplies = ["Flags", "Parade Floats", "Italian Food"]
+
+      add_new_holiday_with_supplies(holiday_supplies, :fall, :columbus_day, columbus_day_supplies)
+
+      expect(holiday_supplies[:fall].keys).to include(:columbus_day)
+      expect(holiday_supplies[:fall][:columbus_day]).to match_array(columbus_day_supplies)
+
+      expect(holiday_supplies[:winter].keys).not_to include(:valentines_day)
+
+      valentines_day_supplies = ["Cupid Cut-Out", "Candy Hearts"]
+      add_new_holiday_with_supplies(holiday_supplies, :winter, :valentines_day, valentines_day_supplies)
+
+      expect(holiday_supplies[:winter].keys).to include(:valentines_day)
+      expect(holiday_supplies[:winter][:valentines_day]).to match_array(valentines_day_supplies)
+    end
+  end
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
 
