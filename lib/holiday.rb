92106d62dad55de4f9215968cfa2020cb9a9d703
Sarah Jacob <smjacob91@gmail.com>
2018-07-07 13:33:06 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -1,24 +1,18 @@
 require 'pry'
 
 def second_supply_for_fourth_of_july(holiday_hash)
-  # given that holiday_hash looks like this:
-  # {
-  #   :winter => {
-  #     :christmas => ["Lights", "Wreath"],
-  #     :new_years => ["Party Hats"]
-  #   },
-  #   :summer => {
-  #     :fourth_of_july => ["Fireworks", "BBQ"]
-  #   },
-  #   :fall => {
-  #     :thanksgiving => ["Turkey"]
-  #   },
-  #   :spring => {
-  #     :memorial_day => ["BBQ"]
-  #   }
-  # }
-  # return the second element in the 4th of July array
+  return holiday_supplies[:spring][:memorial_day]
 end
+
+# Write a method that returns the second supply for the Fourth of July
+  describe "#second_supply_for_fourth_of_july" do
+    it "returns the string 'BBQ' without hardcoding it" do
+      expect(second_supply_for_fourth_of_july(holiday_supplies)).to eq("BBQ")
+      holiday_supplies[:summer][:fourth_of_july][1] = "Watermelon"
+      expect(second_supply_for_fourth_of_july(holiday_supplies)).to eq("Watermelon")
+    end
+  end
+
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
   # holiday_hash is identical to the one above
