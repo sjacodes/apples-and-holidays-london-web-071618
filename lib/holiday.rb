bfef3dc5686459e381fc3f5d6f1002d983466050
Sarah Jacob <smjacob91@gmail.com>
2018-07-07 13:59:17 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -48,7 +48,9 @@ def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_arr
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
-  holiday_hash[season][holiday_name] = supply_array
+  new_hash = {holiday_name => supply_array}
+  holiday_hash[season] = new_has
+  end
   return holiday_hash
 end
 
